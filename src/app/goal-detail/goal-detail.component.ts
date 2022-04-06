c48a7cb3ec2b83b0d7325a6aabf2fb69800ddeca
brayokenya <kiirubrian21@gmail.com>
2022-04-07 00:15:15 +0300

    bootstrapping

--- src/app/goal-detail/goal-detail.component.ts
+++ src/app/goal-detail/goal-detail.component.ts
@@ -16,6 +16,9 @@ export class GoalDetailComponent implements OnInit {
     this.isComplete.emit(complete);
   }
 
+  goalDelete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
   constructor() { }
 
   ngOnInit() {
